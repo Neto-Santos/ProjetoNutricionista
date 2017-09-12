348cf312a919761a7adfbfa733db4ba12268ab66
Neto-Santos <netos0223@gmail.com>
2017-09-11 22:01:54 -0300

    Início do Projeto
    Criando as Primeiras Classes (Models)

---
+++ ProjetoNutricionista/ProjetoNutricionista/Model/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoNutricionista.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Numero { get; set; }
+        public string TipoSanguineo { get; set; }
+    }
+}
---
+++ ProjetoNutricionista/ProjetoNutricionista/Model/Imc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoNutricionista.Model
+{
+    public class Imc
+    {
+        //O Cálculo do IMC se faz dividindo o peso pela altura(em metros) ao quadrado (IMC = 80 ÷ 1,80²)
+        public int Id { get; set; }
+        public  double Peso { get; set; }
+        public double Altura { get; set; }
+    }
+}
